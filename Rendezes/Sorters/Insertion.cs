1476b787bf3791e2c24726b55852820514966a72
Bence Skorka <skorka.bence@gmail.com>
2017-09-12 11:28:19 +0200

    Insertion sort

---
+++ Rendezes/Sorters/Insertion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rendezes.Sorters {
+    class Insertion : ISorter<int> {
+
+
+        public void Sort(ref List<int> input) {
+            for (int i = 1; i < input.Count; i++) {
+                if (input[i] < input[i - 1]) {
+                    int x = input[i];
+                    input[i] = input[i - 1];
+                    int j = i - 2;
+                    while (j >= 0 && input[j] > x) {
+                        input[j + 1] = input[j];
+                        j--;
+                    }
+                    input[j + 1] = x;
+                }
+            }
+        }
+
+
+    }
+}
