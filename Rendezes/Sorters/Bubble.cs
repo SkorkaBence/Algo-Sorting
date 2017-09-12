7ec2fb7d5021255d9d9577d7ef9d23967604cf09
Bence Skorka <skorka.bence@gmail.com>
2017-09-12 11:28:26 +0200

    Bubble sort

---
+++ Rendezes/Sorters/Bubble.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rendezes.Sorters {
+    class Bubble : ISorter<int> {
+
+        public void Sort(ref List<int> input) {
+            for (int i = input.Count - 1; i >= 0; i--) {
+                for (int j = 0; j <= i - 1; j++) {
+                    if (input[j] > input[j + 1]) {
+                        int a = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = a;
+                    }
+                }
+            }
+        }
+
+    }
+}
