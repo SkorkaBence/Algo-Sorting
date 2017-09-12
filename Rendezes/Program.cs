01d395b0ccf5376c147ffd41f6c9945ee0136b29
Bence Skorka <skorka.bence@gmail.com>
2017-09-12 11:28:05 +0200

    Frame

--- Rendezes/Program.cs
+++ Rendezes/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using Rendezes.Sorters;
+using System;
+using System.Collections.Generic;
 
-namespace Rendezes
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Rendezes {
+    class Program {
+
+        static void Main(string[] args) {
+            TestSorter(new Insertion());
+            TestSorter(new Bubble());
+
+            Console.ReadKey();
+        }
+
+        private static void TestSorter(ISorter<int> sorter) {
+            Random r = new Random();
+
+            Console.WriteLine("Testing: " + sorter.GetType().Name);
+
+            List<int> data = new List<int>();
+            List<int> original = new List<int>();
+            List<int> expected = new List<int>();
+
+            while (data.Count < 100) {
+                int c = r.Next(0, 99999);
+                data.Add(c);
+                original.Add(c);
+                expected.Add(c);
+            }
+
+            sorter.Sort(ref data);
+            bool ok = true;
+            for (int i = 1; i < data.Count; i++) {
+                if (data[i] < data[i - 1]) {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (!ok) {
+                expected.Sort();
+                Console.WriteLine("       IN     OUT    EXPECTED STATUS");
+                for (int i = 0; i < data.Count; i++) {
+                    string status = (data[i] == expected[i] ? "OK" : "FAILED");
+                    Console.WriteLine($"{i:000000} {original[i]:000000} {data[i]:000000} {expected[i]:000000}   {status}");
+                }
+                Console.WriteLine();
+            }
+
+            if (ok) {
+                Console.WriteLine("PASSED");
+            } else {
+                Console.WriteLine("FAILED");
+            }
+
+            Console.WriteLine();
         }
     }
 }
---
+++ Rendezes/Sorters/ISorter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rendezes.Sorters {
+
+    interface ISorter<T> {
+
+        void Sort(ref List<T> input);
+
+    }
+}
